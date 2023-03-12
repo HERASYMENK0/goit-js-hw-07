2252796827b4e7d34af9e16789c2b3114ad00851
Maksym <109169487+HERASYMENK0@users.noreply.github.com>
2023-03-13 00:54:45 +0200

    Д/з 6 ч. 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+// import * as basicLightbox from "basiclightbox";
+
 // console.log(galleryItems);
 const imgBox = document.querySelector(".gallery");
 const imgMarkup = creatImagesCard(galleryItems);
@@ -25,9 +28,20 @@ function creatImagesCard(element) {
     }).join(' ')
 };
 function clickImgCard(evt) {
+   evt.preventDefault();
     if (!evt.target.classList.contains('gallery__image')) {
         return
     }
     const largeImageUrl = evt.target.dataset.source;
-    console.log(largeImageUrl);
+    // console.log(largeImageUrl);
+  
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+`);
+
+  instance.show();
+   
+
 }
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const imgBox = document.querySelector(".gallery");
+const imgMarkup = creatImagesCard(galleryItems);
+
+imgBox.insertAdjacentHTML('beforeend', imgMarkup);
+imgBox.addEventListener('click', clickImgCard)
+
+function creatImagesCard(element) {
+    return element.map(({preview, original, description}) => {
+       return `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}"" alt="${description}" />
+</a>
+    `;
+    }).join(' ')
+};
