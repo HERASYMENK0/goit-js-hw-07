ac6630d0fb78b665ae79ead0327dfa29abcac794
Maksym <109169487+HERASYMENK0@users.noreply.github.com>
2023-03-17 00:18:06 +0200

    complited

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,21 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-// console.log(galleryItems);
-
 
 const imgBox = document.querySelector(".gallery");
 const imgMarkup = creatImagesCard(galleryItems);
 
-imgBox.insertAdjacentHTML('beforeend', imgMarkup);
-imgBox.addEventListener('click', clickImgCard)
+imgBox.insertAdjacentHTML("beforeend", imgMarkup);
+imgBox.addEventListener("click", clickImgCard, { passive: false });
 
 function creatImagesCard(element) {
-    return element.map(({preview, original, description}) => {
-       return `
+  return element
+    .map(({ preview, original, description }) => {
+      return `
  <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}"" alt="${description}" />
 </a>
     `;
-    }).join(' ')
-};
+    })
+    .join(" ");
+}
+function clickImgCard(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+   
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionSelector: "img",
+    captionDelay: 250,
+    docClose: true,
+  });
+}
+
